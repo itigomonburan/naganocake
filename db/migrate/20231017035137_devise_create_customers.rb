7ab490197d618f8cfb6718dfb2ba37d72eaac191
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-10-17 08:23:42 +0000

    model作成

--- db/migrate/20231017035137_devise_create_customers.rb
+++ db/migrate/20231017035137_devise_create_customers.rb
@@ -6,6 +6,15 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :phone_namber, null: false
+      t.string :address, null: false
+      t.string :post_code, null: false
+      t.boolean :is_active, null: false, default: true
 
       ## Recoverable
       t.string   :reset_password_token
