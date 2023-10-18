64f409b73f221026ca60447e00f2532db2729e2a
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-10-18 14:13:58 +0000

    [商品新規登録途中]

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,7 @@ class Item < ApplicationRecord
   belongs_to :genle
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
+
+  enum is_active: { 販売中: true, 販売停止中: false }
+
 end
