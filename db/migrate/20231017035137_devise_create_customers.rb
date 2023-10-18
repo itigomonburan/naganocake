e5a136ae6295e1e10b62017154ba0f6819d9555e
19951118 <reonn1118@yahoo.co.jp>
2023-10-18 08:23:46 +0000

    修正後

--- db/migrate/20231017035137_devise_create_customers.rb
+++ db/migrate/20231017035137_devise_create_customers.rb
@@ -11,7 +11,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       t.string :first_name, null: false
       t.string :last_name_kana, null: false
       t.string :first_name_kana, null: false
-      t.string :phone_namber, null: false
+      t.string :phone_number, null: false
       t.string :address, null: false
       t.string :post_code, null: false
       t.boolean :is_active, null: false, default: true
--- db/migrate/20231018034537_rename_phone_namber_column_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class RenamePhoneNamberColumnToCustomers < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :customers, :phone_namber, :phone_number
-  end
-end
