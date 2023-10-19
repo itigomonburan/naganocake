ed0024cd49dea09fb16af249b7b0b9cef86f169b
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-10-19 03:14:29 +0000

    [商品新規登録途中]

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,16 @@
 class Item < ApplicationRecord
 
   has_one_attached :image
-  validates :image, presence:true
-  validates :name, presence:true, uniqueness: true
-  validates :description, presence:true
-  validates :price_without_tax, presence:true, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+
 
   belongs_to :genle
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
+
+  validates :image, presence:true
+  validates :name, presence:true, uniqueness: true
+  validates :description, presence:true
+  validates :price_without_tax, presence:true, numericality: {only_integer: true, greater_than_or_equal_to: 1}
 
   enum is_active: { 販売中: true, 販売停止中: false }
 
