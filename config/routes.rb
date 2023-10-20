ff0c2228f3a7d7a12136c95864fe3cafa17c874c
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-10-20 05:40:57 +0000

    [add]　アドレス　　登録　編集　一覧

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ devise_for :admin, skip: [:registrations, :passwords], controllers: {
     patch 'customers' => 'customers#update'
     get 'customers/check' => 'customers#check'
     patch 'customers/withdraw' => 'customers#withdraw'
+    resources :addresses, only: [:index, :create, :edit, :update, :destroy]
   end
 
 # 管理者ルーティング
