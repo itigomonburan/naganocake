c388b006bb6e134ab0c74fff3e5af6f83d398dc7
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-10-21 02:48:17 +0000

    編集画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,6 +14,16 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+     @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item), notice: "変更しました。"
+    else
+      render :edit
+    end
   end
 
   def create
