498e20859ea4f7a5f9230d5c9292779a087c97a8
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-10-23 12:44:03 +0000

    注文情報入力画面、注文確定画面完了

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,15 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :customer
   has_many :order_details, dependent: :destroy
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
+
+  validates :amount_billed, presence:true
+  validates :shipping_cost, presence:true
+  validates :payment_method, presence:true
+  validates :post_code, presence:true, numericality: {only_integer: true}
+  validates :address, presence:true
+  validates :name, presence:true
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,10 @@
 class OrderDetail < ApplicationRecord
-  
+
   belongs_to :order
   belongs_to :item
+
+  validates :price_including_tax, presence:true
+  validates :amount, presence:true
+
   enum making_status: { start_not_posssible: 0, production_pending: 1, in_production: 2, production_complete: 3 }
 end
