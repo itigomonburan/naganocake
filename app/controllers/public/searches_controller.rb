7efc491ee1d0ef0a439a18f22efe44b7f85da8de
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-10-24 05:16:01 +0000

    編集途中

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,6 +4,7 @@ class Public::SearchesController < ApplicationController
     @genre_id = params[:genre_id]
     @items = Item.where(genre_id: @genre_id).page(params[:page]).per(8)
     @genres = Genre.all
+    @items_all = Item.where(genre_id: @genre_id).count
 # ビュー（ジャンル検索部分）から、送られてきたgenre_idを @genre_idに代入する。
 # @items = Item.where(genre_id: @genre_id) ビュー（ジャンル検索部分）から送られてきたgenre_id`を持つ Itemを全て取得。
   end
@@ -13,9 +14,11 @@ class Public::SearchesController < ApplicationController
     if @content.blank?
       flash[:notice] = '検索キーワードを入力してください。'
       redirect_to request.referer
+    else
+      @records = Item.search_for(@content).page(params[:page]).per(8)
+      @genres = Genre.all
+      @items_all = Item.search_for(@content).count
     end
-    @records = Item.search_for(@content)
-    @genres = Genre.all
   end
 
 end
