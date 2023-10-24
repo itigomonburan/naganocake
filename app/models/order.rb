231ca64aebf45ad250b200b41a312680446acf46
19951118 <reonn1118@yahoo.co.jp>
2023-10-24 06:02:47 +0000

    管理者注文詳細50%

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,12 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
+  
+  def subtotal
+    item.with_tax_price * amount
+  end
+  
+  def with_tax_price
+    (price * 1.1).floor
+  end
 end
